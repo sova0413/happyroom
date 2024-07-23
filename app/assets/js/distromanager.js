d83bf80ff3526d455681b07ec2880cd8a1ea4525
BabyBear7 <soul7687@gmail.com>
2024-07-23 15:32:06 +0900

    distro link update

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://github.com/sova0413/happyroom-distro/blob/7d2d80e58ce76404d6040196eeae249b3c26ac91/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://github.com/sova0413/happyroom-distro/blob/main/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
